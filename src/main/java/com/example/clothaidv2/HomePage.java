9eb979f2313494b21b906e9a472b831eef683d70
mehulm30 <mehulmore2005@gmail.com>
2023-10-20 16:45:47 +0530

    Final

--- src/main/java/com/example/clothaidv2/HomePage.java
+++ src/main/java/com/example/clothaidv2/HomePage.java
@@ -1,4 +1,22 @@
-package com.example.clothaidv2;
+package com.example.tmkc;
 
-public class HomePage {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomePage implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //
+    }
+    @FXML
+    public void setLogout(ActionEvent event){
+        scenechanger.changeScene(event,"login.fxml","Login","");
+    }
 }
--- src/main/java/com/example/clothaidv2/LoginPage.java
+++ src/main/java/com/example/clothaidv2/LoginPage.java
@@ -1,66 +1,51 @@
-package com.example.clothaidv2;
+package com.example.tmkc;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.Button;
-import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
 
-import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LoginPage  implements Initializable {
+
+public class LoginPage implements Initializable {
+
     @FXML
-    private Button LoginBtn;
+    private TextField txt_user;
     @FXML
     private Label messageInvalid;
     @FXML
-    private ImageView donateImageView;
+    private PasswordField txt_pass;
     @FXML
-    private ImageView logoImageView;
-    @FXML
-    private TextField usernameTF;
-    @FXML
-    private PasswordField passTF;
+    private Button bt_login;
+
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle){
-        File donateimg  = new File("src/main/resources/com/example/clothaidv2/images/login-img.png");
-        Image donate_image = new Image(donateimg.toURI().toString());
-        donateImageView.setImage(donate_image);
-        //
-        File logo  = new File("src/main/resources/com/example/clothaidv2/images/img.png");
-        Image logoImg = new Image(logo.toURI().toString());
-        logoImageView.setImage(logoImg);
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+
 
     }
-    public void setLoginBtnOnAction(ActionEvent event){
-        //
-        if(usernameTF.getText().isBlank() == false && passTF.getText().isBlank() == false){
-            messageInvalid.setText("You try to login.");
-            validLogin();
+    @FXML
+    public void setBt_login(ActionEvent event){
+        if(!txt_user.getText().isBlank() && !txt_pass.getText().isBlank())
+        {
+            dataBase.loginUser(event, txt_user.getText(), txt_pass.getText());
+//            wait(3000);
         }
         else{
-            messageInvalid.setText("You try to login.");
+            messageInvalid.setText("Username and Password is Blank");
         }
-    }
-    public void setLoginBtnOnAmessageInvalidction(ActionEvent event) {
+
 
     }
-    @FXML
-    private Button SignUpBtn;
-    public void setSignUpBtnLoginPageonAction(ActionEvent event){
-        Stage stage = (Stage) SignUpBtn.getScene().getWindow();
-    }
-
-    public void validLogin(){
+        @FXML
+    public void setBt_SignUp(ActionEvent event){
+        scenechanger.changeScene(event,"signup.fxml","Sign Up","");
 
     }
-
 }
